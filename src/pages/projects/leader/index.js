cbd51a664e5b1ceafdb79ced2a76a58c9fc47486
Nikita Zolotov <nikzolotov@gmail.com>
2020-06-05 10:37:33 +0300

    Add Leader text

--- src/pages/projects/leader/index.js
+++ src/pages/projects/leader/index.js
@@ -16,22 +16,44 @@ export default ({ data }) => {
       <SEO title="Leader Engineering" />
       <LayoutColumn>
         <ArticleTitle title="Leader Engineering" meta="2016 · Web" />
+        <p>
+          Leader is a Moscow based engineering company. They offer their clients
+          services in civil, electrical, mechanical, piping, and structural
+          engineering. The goals of the redesign were to attract new customers
+          and show new specialties.
+        </p>
+        <p>
+          Competition in the field is tough. Although, competitor sites are
+          rather similar. Portfolio, testimonials, licenses, and description of
+          the service. To attract new customers and improve SEO metrics, we
+          decided to add new sections with articles and normative documents in
+          the field. The client already had a nice collection of unique articles
+          and had a desire to write more. And we updated key product pages with
+          new data.
+        </p>
       </LayoutColumn>
-      <Frame label="Main screen and outlet settings">
+      <Frame label="Main page">
         <Device
           image={filterImage(images, "leader-main")}
           model="safari"
           margin="0"
         />
       </Frame>
-      <Frame label="Main screen and outlet settings">
+      <Frame label="Product page">
         <Device
           image={filterImage(images, "leader-product")}
           model="safari"
           margin="0"
         />
       </Frame>
-      <Frame label="Main screen and outlet settings">
+      <LayoutColumn>
+        <p>
+          Leader has a nice portfolio with famous buildings in the center of
+          Moscow and well-known areas of gentrification. I couldn't ignore this
+          and made attractive project pages and cross-links on other pages.
+        </p>
+      </LayoutColumn>
+      <Frame label="Project page">
         <Device
           image={filterImage(images, "leader-project")}
           model="safari"
